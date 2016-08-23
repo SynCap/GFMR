741a4223ef6e484e513f663e5b4f69e0df0b0ae0
SynCap <syncap@ya.ru>
2016-08-23 18:52:33 +0800

    #2 remaster gulp + code striped bg

--- gulpfile.js
+++ gulpfile.js
@@ -11,21 +11,27 @@
  * 		default task - `build`
  */
 
-// const chalk  = require('chalk');
-const gulp   = require('gulp');
-const concat = require('gulp-concat-util');
-const csso   = require('gulp-csso');
-// const debug  = require('gulp-debug');
-const less   = require('gulp-less');
-// const rename = require('gulp-rename');
-const path   = require('path');
-const pump   = require('pump');
-const uglify = require('gulp-uglify');
-// const gccr   = require('gulp-closure-compiler');
-// const watch = require('gulp-watch');
+const path    = require('path');
+const pump    = require('pump');
+const del     = require('del');
+const gulp    = require('gulp');
+const concat  = require('gulp-concat-util');
+const less    = require('gulp-less');
+const uglify  = require('gulp-uglify');
+const debug   = require('gulp-debug');
+const srcmaps = require('gulp-sourcemaps');
+// const autoprefixer = require('gulp-autoprefixer');
+// const csso         = require('gulp-csso');
+// const cleancss     = require('gulp-clean-css');
+// const chalk        = require('chalk');
+// const gccr         = require('gulp-closure-compiler');
+// const rename       = require('gulp-rename');
+// const stripDebug   = require('gulp-strip-debug');
+// const vinylPaths   = require('vinyl-paths');
+// const watch        = require('gulp-watch');
 
 
-var libPath = {
+const srcPath = {
 	styles : './dev/less/gfmr.less',
 	hl : {
 		dir : './dev/js/hl/build',
@@ -59,55 +65,70 @@ var destPath = {
 	return chalk.gray((new Date()).toLocaleString().slice(11));
 }*/
 
-gulp.task('hl', function (cb) {
+gulp.task('js:hl', function (cb) {
 	pump([
-		gulp.src(libPath.hl.files, {cwd: libPath.hl.dir}),
+		gulp.src(srcPath.hl.files, {cwd: srcPath.hl.dir}),
 		// debug({minimal:false}),
+		srcmaps.init(),
+		uglify(uglifyOptions),
 		concat('hl.js'),
-		uglify(uglifyOptions),
+		srcmaps.write(),
 		gulp.dest(destPath.js)
 	], cb);
 });
 
-gulp.task('mdi', function (cb) {
+gulp.task('js:mdi', function (cb) {
 	pump([
-		gulp.src(libPath.mdi.files, {cwd: libPath.mdi.dir}),
+		gulp.src(srcPath.mdi.files, {cwd: srcPath.mdi.dir}),
 		// debug({minimal:false}),
-		concat('mdi.js'),
+		srcmaps.init(),
 		uglify(uglifyOptions),
+		concat('mdi.js'),
+		srcmaps.write(),
 		gulp.dest(destPath.js)
 	], cb);
 });
 
-gulp.task('init', function (cb) {
+gulp.task('js:init', function (cb) {
+	del.sync([path.join(destPath.js),'init.js*']);
 	pump([
 		gulp.src( 'init.js', { cwd: './dev/js' }),
 		// debug({minimal: false}),
+		srcmaps.init(),
 		uglify(uglifyOptions),
+		srcmaps.write('./'),
 		gulp.dest(destPath.js)
 	], cb);
 });
 
-gulp.task('css', function (cb) {
+gulp.task('clean:css', function(cb){
+	return del(['src/css/**/*']);
+});
+
+gulp.task('css', /*gulp.series('clean:css'),*/ function (cb) {
 
 	var LessAutoprefix = require('less-plugin-autoprefix');
-	var autoprefix = new LessAutoprefix({ browsers: ['last 2 versions'] });
+	var autoprefix = new LessAutoprefix({ browsers: ['chrome > 25', 'opera > 12'] });
 
 	var LessCleanCSS = require('less-plugin-clean-css');
 	var cleanCss = new LessCleanCSS({advanced: true});
 
-	var LessLesshat = require('less-plugin-less'),
-    lesshat = new LessLesshat();
+	/*var LessLesshat = require('less-plugin-lesshat'),
+    lesshat = new LessLesshat();*/
 
-	pump([
-		gulp.src( libPath.styles ),
-		// debug({minimal:false}),
-		// less({plugins: [autoprefix, cleanCss, lesshat]}),
-		less(),
-		csso(),
-		gulp.dest(destPath.css)
+	// return gulp.src( srcPath.styles )
+	pump([ gulp.src( srcPath.styles )
+		, debug({title: 'CSS files'})
+		, srcmaps.init()
+		, less({plugins: [autoprefix, cleanCss]})
+		// , less()
+		// , autoprefixer()
+		// , csso()
+		, srcmaps.write('./')
+		, gulp.dest(destPath.css)
 	], cb);
+	;
 });
 
-gulp.task('js', ['hl','mdi','init']);
-gulp.task('default', ['js']);
+gulp.task('js:all', gulp.parallel('js:hl','js:mdi','js:init'));
+gulp.task('default', gulp.parallel('css','js:all'));
