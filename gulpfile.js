c5f6c6a1d848d7ace0d05a9d72caeeb60eea8338
SynCap <syncap@ya.ru>
2016-09-22 10:00:00 +0800

    default task == build

--- gulpfile.js
+++ gulpfile.js
@@ -198,8 +198,9 @@ gulp.task('css', /*gulp.series('css:clean'),*/ function (cb) {
 });
 
 gulp.task('js:all', gulp.parallel('js:hl','js:mdi','js:init'));
-gulp.task('build', gulp.parallel('css', 'js:all'));
-gulp.task('default', gulp.parallel('css', 'js:init'));
+gulp.task('build', gulp.parallel('css', 'js:init'));
+gulp.task('build:all', gulp.parallel('css', 'js:all'));
+gulp.task('default', gulp.parallel('build'));
 
 gulp.task('vigil', function (done) {
 	gulp.watch(srcPath.styles.files, {cwd: srcPath.styles.dir}, gulp.parallel('css') );
